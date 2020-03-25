5257c9b57070c24e673d28faa8b6da2c21b05e0c
PMack2 <Prestonmack@gmail.com>
2020-03-25 13:59:37 -0400

    fixed date on USA4.html

--- states4.js
+++ states4.js
@@ -18,13 +18,14 @@ fetch("https://covidtracking.com/api/states", {
         
         //we will start by inserting the new rows inside our table
         let row = table.insertRow(i+1);
-        let state = row.insertCell(0);
-        let positive = row.insertCell(1);
-        let negative = row.insertCell(2);
-        let hospitalized = row.insertCell(3);
-        let death = row.insertCell(4);
-        let total = row.insertCell(5);
-        let dateChecked = row.insertCell(6);
+        let lastUpdateEt = row.insertCell(0);
+        let state = row.insertCell(1);
+        let positive = row.insertCell(2);
+        let negative = row.insertCell(3);
+        let hospitalized = row.insertCell(4);
+        let death = row.insertCell(5);
+        let total = row.insertCell(6);
+        
 
         state.innerHTML = data[i].state;
         positive.innerHTML = data[i].positive;
@@ -32,7 +33,7 @@ fetch("https://covidtracking.com/api/states", {
         hospitalized.innerHTML = data[i].hospitalized;
         death.innerHTML = data[i].death;
         total.innerHTML = data[i].total;
-        dateChecked.innerHTML = data[i].dateChecked;
+        lastUpdateEt.innerHTML = data[i].lastUpdateEt;
       
 
     }
