e5bfcafcb87acc0eac78e8b975b761da0c6166ae
PMack2 <Prestonmack@gmail.com>
2020-03-25 12:41:33 -0400

    added USA daily stats page

--- statesdaily.js
+++ statesdaily.js
@@ -2,7 +2,7 @@ let table = document.getElementById('usa')
 
 
 
-fetch("https://covidtracking.com/api/states", {
+fetch("https://covidtracking.com/api/us/daily", {
 	"method": "GET",
 
 })
@@ -18,21 +18,21 @@ fetch("https://covidtracking.com/api/states", {
         
         //we will start by inserting the new rows inside our table
         let row = table.insertRow(i+1);
-        let state = row.insertCell(0);
+        let date = row.insertCell(0);
         let positive = row.insertCell(1);
         let negative = row.insertCell(2);
-        let hospitalized = row.insertCell(3);
-        let death = row.insertCell(4);
-        let total = row.insertCell(5);
-        let dateChecked = row.insertCell(6);
+        let pending = row.insertCell(3);
+        let hospitalized = row.insertCell(4);
+        let death = row.insertCell(5);
+        let total = row.insertCell(6);
 
-        state.innerHTML = data[i].state;
+        date.innerHTML = data[i].date;
         positive.innerHTML = data[i].positive;
         negative.innerHTML = data[i].negative;
+        pending.innerHTML = data[i].pending;
         hospitalized.innerHTML = data[i].hospitalized;
         death.innerHTML = data[i].death;
         total.innerHTML = data[i].total;
-        dateChecked.innerHTML = data[i].dateChecked;
       
 
     }
